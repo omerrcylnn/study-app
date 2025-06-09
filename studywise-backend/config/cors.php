6e6072b285d4a68b1953011ad99bbb8db409b4a6
Borstblade1-sudo <ceylangoneromer@gmail.com>
2025-06-10 00:40:08 +0300

    fix: cors ayarları güncellendi.

--- studywise-backend/config/cors.php
+++ studywise-backend/config/cors.php
@@ -2,7 +2,7 @@
 
 return [
     'paths' => ['register', 'login', 'logout','sanctum/csrf-cookie','api/*'],
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:5173','https://study-app-tawny.vercel.app/'],
     'allowed_headers' => ['*'],
     'allowed_methods' => ['*'],
     'supports_credentials' => true,
