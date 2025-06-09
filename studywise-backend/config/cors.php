a5d6f29d96af23c7754cf9169ec60f89aada3ac8
Borstblade1-sudo <ceylangoneromer@gmail.com>
2025-06-10 01:09:50 +0300

    fix: Cors

--- studywise-backend/config/cors.php
+++ studywise-backend/config/cors.php
@@ -1,9 +1,19 @@
 <?php
 
 return [
-    'paths' => ['register', 'login', 'logout','sanctum/csrf-cookie','api/*'],
-    'allowed_origins' => ['http://localhost:5173','https://study-app-tawny.vercel.app/'],
-    'allowed_headers' => ['*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
+
     'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['https://study-app-tawny.vercel.app'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
     'supports_credentials' => true,
 ];
