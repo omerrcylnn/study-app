7f9fd011d93abe3492dd34301c490033e405c783
Borstblade1-sudo <ceylangoneromer@gmail.com>
2025-06-11 14:30:15 +0300

    fix

--- studywise-backend/config/cache.php
+++ studywise-backend/config/cache.php
@@ -11,7 +11,7 @@ return [
     |
     | This option controls the default cache store that will be used by the
     | framework. This connection is utilized if another isn't explicitly
-    | specified when running a cache operation inside the application.
+    | specified when running a cache operation inside the application.xd
     |
     */
 
